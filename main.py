236c1f0fa7a03d82ed88bb94921faaab38b595e4
AljazPocic <pocic.aljaz@gmail.com>
2025-08-29 16:52:54 +0200

    dodal zdruzi_vse_kategorije('podatki') v main ter napisel kretek opis datoteke

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import zajem
 import obdelava
 import shrani
 
+# Program zajame podatke, jih obdela in ustvari 7 CSV datotek. Najbolj pomembna je vse_kategorije.csv
+
 def main():
     print("Zajem spletne strani")
     for kategorija in zajem.podjetja_urls:
@@ -10,6 +12,7 @@ def main():
 
     print("Obdelava podatkov in shranjevanje v CSV")
     shrani.shrani_vse_kategorije('podatki') #obdelva je že vlkjučena v shrani.py
+    shrani.zdruzi_vse_kategorije('podatki')
     print("Obdelva in shranjevannje je končano!")
 
 if __name__ == "__main__":
